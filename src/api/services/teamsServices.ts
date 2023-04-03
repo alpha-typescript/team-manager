09330fd1bfae4bb91106da4fd62c02ec19c95cf3
Felipe Fernandes <felipe.bravil@gmail.com>
2023-04-02 22:03:18 -0300

    get all users feature added | minor authentication verifiers to other features

--- src/api/services/teamsServices.ts
+++ src/api/services/teamsServices.ts
@@ -1,17 +1,47 @@
 import IResult from "../../interfaces/iResult";
 import ITeam from "../../interfaces/iTeam";
+import IUser from "../../interfaces/iUser";
 import teamsRepositories from "../repositories/teamsRepositories";
 class TeamsServices {
-    async list(): Promise<IResult<ITeam[]>> {
+    async list(user: IUser): Promise<IResult<ITeam[]>> {
         let result: IResult<ITeam[]> = { errors: [], status: 200 };
         try {
-            result = await teamsRepositories.list();
+            if (user.isAdmin || user.isLeader) {
+                result = await teamsRepositories.list();
+            } else {
+                throw new Error("User doesn't have permission");
+            }
         } catch (error: any) {
             result.errors?.push(error.message);
-            result.status = 500;
+            if (error.message === "User doesn't have permission") {
+                result.status === 401;
+            } else {
+                result.status = 500;
+            }
         }
         return result;
     }
+
+    async getTeam(teamId: string, user: IUser): Promise<IResult<ITeam>> {
+        let result: IResult<ITeam> = { errors: [], status: 200 };
+        try {
+            const team = await teamsRepositories.find(teamId);
+            if (user.isAdmin || user.isLeader || user.team === team.data?.id) {
+                result = team;
+            } else {
+                throw new Error("User doesn't have permission");
+            }
+        } catch (error: any) {
+            result.errors?.push(error.message);
+            if (error.message === "User doesn't have permission") {
+                result.status === 401;
+            } else {
+                result.status = 500;
+            }
+        }
+        return result;
+    }
+
     /*     async insert(product: IProduct): Promise<IResult<IProduct>> {
         const pool = await Postgres.pool();
         let result: IResult<IProduct> = { errors: [], status: 200 };
