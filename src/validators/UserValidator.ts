d6a6f6b139d214dbadc0d9cd1e297d04901031a6
Letonio <letoniojs@gmail.com>
2023-04-04 22:54:35 -0300

    upd: Validators is working at routes GET users and GET teams

--- src/validators/UserValidator.ts
+++ src/validators/UserValidator.ts
@@ -2,43 +2,43 @@ import { Request, Response, NextFunction } from "express";
 import Validator from "./validator";
 
 export default function UserValidator(
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) {
-  let errors: string[] = (req.query.errors as string[]) || [];
-  const { username, firstName, lastName, email, password, squad, isAdmin } =
-    req.body;
+    let errors: string[] = (req.query.errors as string[]) || [];
+    const { username, firstName, lastName, email, password, squad, isAdmin } =
+        req.body;
 
-  if (username) {
-    errors = [...errors, ...Validator.isValidName(username)];
-  }
+    if (username) {
+        errors = [...errors, ...Validator.isValidName(username)];
+    }
 
-  if (email) {
-    errors = [...errors, ...Validator.isValidEmail(email)];
-  }
+    if (email) {
+        errors = [...errors, ...Validator.isValidEmail(email)];
+    }
 
-  if (firstName) {
-    errors = [...errors, ...Validator.isValidName(firstName)];
-  }
+    if (firstName) {
+        errors = [...errors, ...Validator.isValidName(firstName)];
+    }
 
-  if (lastName) {
-    errors = [...errors, ...Validator.isValidName(lastName)];
-  }
+    if (lastName) {
+        errors = [...errors, ...Validator.isValidName(lastName)];
+    }
 
-  if (password) {
-    errors = [...errors, ...Validator.isValidPassword(password)];
-  }
+    if (password) {
+        errors = [...errors, ...Validator.isValidPassword(password)];
+    }
 
-  if (squad) {
-    errors = [...errors, ...Validator.isValidId(squad)];
-  }
+    if (squad) {
+        errors = [...errors, ...Validator.isValidId(squad)];
+    }
 
-  if (isAdmin) {
-    errors = [...errors, ...Validator.isValidBool(isAdmin)];
-  }
+    if (isAdmin) {
+        errors = [...errors, ...Validator.isValidBool(isAdmin)];
+    }
 
-  req.query.errors = errors;
+    req.query.errors = errors;
 
-  next();
+    next();
 }
